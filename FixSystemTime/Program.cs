16ff3f876c34514e1955068481c0831e56da46d4
Marlon Petit <marlon.petit@epitech.eu>
2018-11-17 08:32:47 +0100

    Add project files.

---
+++ FixSystemTime/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace FixSystemTime
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime(ref SystemTime st);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SystemTime
+        {
+            public short wYear;
+            public short wMonth;
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            public short wMilliseconds;
+        }
+
+        public static DateTime GetNistTime()
+        {
+            DateTime dateTime = DateTime.MinValue;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://nist.time.gov/actualtime.cgi?lzbc=siqm9b");
+            request.Method = "GET";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore); //No caching
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader stream = new StreamReader(response.GetResponseStream());
+                string html = stream.ReadToEnd();//<timestamp time=\"1395772696469995\" delay=\"1395772696469995\"/>
+                string time = Regex.Match(html, @"(?<=\btime="")[^""]*").Value;
+                double milliseconds = Convert.ToInt64(time) / 1000.0;
+                dateTime = new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+            }
+
+            return dateTime;
+        }
+
+        private static void Main()
+        {
+            var date = GetNistTime();
+            var st = new SystemTime
+            {
+                wYear = (short)date.Year,
+                wMonth = (short)date.Month,
+                wDay = (short)date.Day,
+                wHour = (short)date.Hour,
+                wMinute = (short)date.Minute,
+                wSecond = (short)date.Second,
+                wDayOfWeek = (short)date.DayOfWeek,
+                wMilliseconds = (short)date.Millisecond
+            };
+            if (st.wHour == 0)
+                st.wHour = 23;
+            else
+                st.wHour = (short) (st.wHour - 1);
+
+            SetSystemTime(ref st); // invoke this method.
+        }
+    }
+}
